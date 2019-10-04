9a178e9e1704913d6b0fb456458bea8aebcc5032
piotr-andrzej-bochenek <piotr8207@interia.eu>
2019-10-04 20:57:31 +0200

    task_03 initialized

---
+++ zadanie_03/scripts.js
@@ -0,0 +1,68 @@
+/****************************************************************************************************
+ 1. Dodaj poprawnie plik 'script.js' do pliku index.html. Wyświetl w konsoli: 'Poprawnie dodany skrypt';
+
+ 2. Napisz funkcję, która przyjmuje 2 argumenty i zwraca większy z nich.
+ Input: 2, 5        Output: 5
+ Input: 2, -5       Output: 2
+ Input: 'aaa', -5   Output: 'argumenty to nie liczby`
+
+ 3. Napisz funkcję, która zwraca stringa z dużą pierwszą literę.
+ Input: uppercaseFirstLetter('kamil') Output: '4.1.1: Kamil'
+ Input: uppercaseFirstLetter(12)      Output: '4.1.2: to nie jest string'
+
+ 4. Wyświetl wartość w konsoli z obiektu labyrinth:
+ a) 'Garfield'
+ b) 'elephant'
+ c) 'flying'
+ Input: labirynth = {
+        animals: [
+            { name: 'Pluto', species: 'dog' },
+            { name: 'Garfield', species: 'cat' },
+            { name: 'Simba', species: 'lion' },
+            { name: 'Maniek', species: 'elephant' }
+        ],
+        nonAnimals: [
+            { name: 'R2D2', species: 'robot', skills: ['fixing', 'flying'] },
+            { name: 'C3PO', species: 'robot', skills: ['talking'] }
+        ],
+        showAnimals: function() { console.log(this.animals) },
+        showNonAnimals: function() { console.log(this.nonAnimals) }
+    }
+ Output: 'a) Garfield b) elephant c) flying'
+
+ 5. Napisz pętle, która iteruje od 1 do 50 i na każdej wielokrotności 3 wyświetl Fizz, na każdej wielokrotności 5 Buzz,
+ a na wielokrotności 3 i 5 FizzBuzz.
+ Output: 'Fizz Buzz Fizz Fizz Buzz Fizz FizzBuzz Fizz Buzz Fizz Fizz Buzz Fizz FizzBuzz Fizz Buzz Fizz Fizz Buzz Fizz FizzBuzz Fizz Buzz'
+
+ 6. Napisz funkcję, która przyjmuje 2 parametry (znak, liczbę), która wyświetli ciąg znaków połączony '-' ale nie więcej niż 10 znaków.
+ Input: $, 5         Output: $-$-$-$-$
+ Input: $, 17        Output: $-$-$-$-$-$-$-$-$-$
+
+ 7. Napisz pętle wyświetlającą nazwę pizzy i jej składniki w nawiasie oraz po myślniku cenę.
+ Input: [
+ {name: 'Margarita', price: 15, ingredients: ['ser', 'sos', 'oregano']},
+ {name: 'Roma', price: 18, ingredients: ['ser', 'sos', 'szynka', 'pieczarki']},
+ {name: 'Salami', price: 18, ingredients: ['ser', 'sos', 'salami']}
+ ]
+ Output:
+ 'Margarita (ser, sos, oregano) - 15
+ Roma (ser, sos, szynka, pieczarki) - 18
+ Salami (ser, sos, salami) - 18'
+
+ 8. Napisz funkcję, która przyjmuje 3 parametry (2x number, 1x string jako operator (dostępne +,-,/,*) i zwraca wartość wyniku:
+ Input: 1, '+', 2    Output: '2.3.1: 3'
+ Input: 20, '-', 3   Output: '2.3.2: 17'
+ Input: -1, '*', 8   Output: '2.3.3: -8'
+ Input: 4, '/', 2    Output: '2.3.4: 2'
+ Input: 5, '!', 5    Output: '2.3.5: Nie znany operator'
+ Input: 4, '/', 0    Output: '2.3.6: Nie można dzielić przez zero'
+
+ 9*. Napisz pętle, która zwróci dany ciąg znaków w konsoli (dokładnie w takiej strukturze).
+ Output:
+ *
+ * *
+ * * *
+ * * * *
+ * * * * *
+
+ */
