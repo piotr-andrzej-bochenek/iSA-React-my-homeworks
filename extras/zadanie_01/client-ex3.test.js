43b6697a87e9e5841a752614f17058cb6f80d994
Kamil Richert <kamil.richert92@gmail.com>
2019-11-12 21:06:16 +0100

    Added extra ex 1

---
+++ extras/zadanie_01/client-ex3.test.js
@@ -0,0 +1,72 @@
+// EX 3. Let's write some tests for class methods
+//  a) Something is missing for `getNextOrderNumber`. What will happened if client does not have any order as undefined 🙀
+//     Write a test which verifies how it should behave, and then fix it in code! 
+//     The correct order number for `undefined` orders should be "ISA-#1" 
+//  b) `calculateDiscount` needs some tests - please write them. Notice the `ClientService` construtor!
+//  c) New requirements arrived! Now if client type is `INTERNAL` he has extra +15% discount! (sums up with existing discounts)
+//      Write test first, and then implement this new logic in `calculateDiscount` method (it is 1 extra `if` 😉)
+//  d) Let's write 2-3 basic tests for `calculateNewOrderValue`
+
+describe('getNextOrderNumber', () => {
+    test('should generate correct number if client already made 2', () => {
+        // arrange
+        const service = new ClientService();
+
+        const client = {
+            orders: [100, 200],
+            type: 'EXTERNAL'
+        };
+
+        // act
+        const actual = service.getNextOrderNumber(client);
+
+        // assert
+        if (actual === 'ISA-#3') {
+            return;
+        } else {
+            throw new Error(`Assertion failed! Actual="${actual}", where should be "ISA-#3"`);
+        }
+    });
+});
+
+describe('calculateDiscount', () => {
+    test('should return 0 for orders less than 500', () => {
+        // arrange
+        const service = new ClientService();
+
+        const client = {
+            orders: [100, 50, 100, 20],
+            type: 'EXTERNAL'
+        };
+
+        // act
+        const actual = service.calculateDiscount(client);
+
+        // assert
+        if(actual === 0) {
+            return;
+        } else {
+            throw new Error('Assertion failed');
+        }
+    });
+
+    test('should return 0.1 for orders higher than 500', () => {
+        // arrange
+        const service = new ClientService();
+
+        const client = {
+            orders: [100, 500, 100, 200],
+            type: 'EXTERNAL'
+        };
+
+        // act
+        const actual = service.calculateDiscount(client);
+
+        // assert
+        if(actual === 0.1) {
+            return;
+        } else {
+            throw new Error('Assertion failed');
+        }
+    });
+});
