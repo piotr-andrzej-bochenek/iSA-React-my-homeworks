bce3d975428f3d3833a34cf73dcffd05cc56c2fd
piotr-andrzej-bochenek <piotr8207@interia.eu>
2019-12-15 12:05:25 +0100

    players.js added, filled with arr of obj, exported

---
+++ zadanie_08/my-app/src/players.js
@@ -0,0 +1,46 @@
+import React from 'react';
+
+const players = [
+    {
+        username: '',
+        points: 0,
+    },
+    {
+        username: '',
+        points: 0,
+    },
+    {
+        username: '',
+        points: 0,
+    },
+    {
+        username: '',
+        points: 0,
+    },
+    {
+        username: '',
+        points: 0,
+    },
+    {
+        username: '',
+        points: 0,
+    },
+    {
+        username: '',
+        points: 0,
+    },
+    {
+        username: '',
+        points: 0,
+    },
+    {
+        username: '',
+        points: 0,
+    },
+    {
+        username: '',
+        points: 0,
+    },
+];
+
+export default players;
