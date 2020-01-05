7feccc4e813fad0a1ed2827b9c3d3870615bf268
piotr-andrzej-bochenek <piotr8207@interia.eu>
2020-01-05 22:01:54 +0100

    copy relevant files from task 8

--- zadanie_09/my-app/src/App.js
+++ zadanie_09/my-app/src/App.js
@@ -1,24 +1,111 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import players from './players.json'
+
+const pointsTotal = players.reduce((total, current) => {
+  return (total + current.points)
+  },0);
+
+class RenderPlayersTable extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      highlightingColor: '',
+      backgroundColorChanged: false,
+    };
+  }
+
+  render() {
+    return (
+      <div className='App'>
+        <div className='App__header'>
+          Click here to highlight players with scores equal and above 100 points.
+        </div>
+        <div className='App__buttons'>
+          <button
+            style = {{backgroundColor: 'red'}}
+            onClick = {() => this.setState({
+                    highlightingColor: 'red',
+                    backgroundColorChanged: true,
+          })}>
+            Red
+          </button>
+          
+          <button
+            style = {{backgroundColor: 'blue'}}
+            onClick = {() => this.setState({
+                    highlightingColor: 'blue',
+                    backgroundColorChanged: true,
+          })}>
+            Blue
+          </button>
+
+          <button onClick = {() => this.setState({
+                    highlightingColor: "",
+                    backgroundColorChanged: false,
+          })}>
+            None
+          </button>
+        </div>
+
+        <table className='App__table'>
+          
+          <thead >
+            <tr>
+              <th> User name </th>
+              <th> Points </th>
+            </tr>
+          </thead>
+
+          <tbody>
+            {
+              players.map((player, index) => {
+              if (player.points < 100) {
+                return (
+                  <tr key = {index}>
+                    <td> {player.userName} </td>
+                    <td> {player.points} </td>
+                  </tr>
+                  )} else {
+                  return (
+                    <tr
+                      style={{
+                        backgroundColor: `${this.state.highlightingColor}`,
+                        color: this.state.backgroundColorChanged ? 'white' : '',
+                      }}
+                      key = {index}
+                    >
+                      <td> {player.userName} </td>
+                      <td> {player.points} </td>
+                    </tr>
+                  )};
+              })
+            }
+          </tbody>
+
+          <tfoot>
+            <tr>
+              <td> Total </td>
+              <td> {pointsTotal} </td>
+            </tr>
+            <tr>
+              <td>Average</td>
+              <td> {pointsTotal / players.length} </td>
+            </tr>
+
+          </tfoot>
+
+        </table>
+      </div>
+    );
+  };
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <RenderPlayersTable />
     </div>
   );
 }
