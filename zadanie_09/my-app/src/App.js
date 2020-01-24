aaa74425728e81f90e2cfb3121a9f2a42f0170a5
piotr-andrzej-bochenek <piotr8207@interia.eu>
2020-01-24 22:00:51 +0100

    9.1 add fetch from players.json, apply state references where relevant

--- zadanie_09/my-app/src/App.js
+++ zadanie_09/my-app/src/App.js
@@ -1,26 +1,53 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
-import players from './players.json'
-
-const pointsTotal = players.reduce((total, current) => {
-  return (total + current.points)
-  },0);
 
 class RenderPlayersTable extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
+      players: [],
+      sumOfPoints: 0,
       highlightingColor: '',
       backgroundColorChanged: false,
+      isLoading: true,
+      error: false,
     };
+  };
+
+  getPlayers = () => {
+    fetch('/data/players.json')
+    .then( response => response.json())
+    .then( fetchedData => {
+      this.setState({ 
+        players: fetchedData,
+       });
+    })
+    .then( ()  => {
+      this.setState({ 
+        sumOfPoints: this.state.players.reduce( (total, current) => total + current.points, 0),
+       });
+    })
+    .catch( error => this.setState({ error: error }))
+    .finally( () => this.setState({ isLoading: false }));
+  };
+
+  componentDidMount() {
+    this.getPlayers();
   }
 
   render() {
+    if (this.state.isloading) {
+      return <h1>Loading ...</h1>;
+    };
+
+    if (this.state.error) {
+      return <h1>{`An error occurred: ${this.state.error}.`}</h1>;
+    };
+
     return (
       <div className='App'>
         <div className='App__header'>
-          Click here to highlight players with scores equal and above 100 points.
+          Click buttons to highlight (or clear highlight of) players with scores equal and above 100 points.
         </div>
         <div className='App__buttons'>
           <button
@@ -48,7 +75,7 @@ class RenderPlayersTable extends React.Component {
             None
           </button>
         </div>
-
+        
         <table className='App__table'>
           
           <thead >
@@ -59,8 +86,8 @@ class RenderPlayersTable extends React.Component {
           </thead>
 
           <tbody>
-            {
-              players.map((player, index) => {
+            { 
+              this.state.players.map((player, index) => {
               if (player.points < 100) {
                 return (
                   <tr key = {index}>
@@ -87,13 +114,12 @@ class RenderPlayersTable extends React.Component {
           <tfoot>
             <tr>
               <td> Total </td>
-              <td> {pointsTotal} </td>
+              <td> {this.state.sumOfPoints} </td>
             </tr>
             <tr>
-              <td>Average</td>
-              <td> {pointsTotal / players.length} </td>
+              <td> Average </td>
+              <td> {Math.round(this.state.sumOfPoints / this.state.players.length)} </td>
             </tr>
-
           </tfoot>
 
         </table>
