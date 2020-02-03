85e507f55e30ac92526ee30bae9e2b84aa14ead7
piotr-andrzej-bochenek <piotr8207@interia.eu>
2020-02-03 22:42:25 +0100

    10.1 build my-app with ToDo component

---
+++ zadanie_10/my-app/src/App.js
@@ -0,0 +1,15 @@
+import * as React from 'react';
+
+import ToDo from './components/toDo/ToDo';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <h1>What needs to be done:</h1>
+      <ToDo />
+    </div>
+  );
+}
+
+export default App;
---
+++ zadanie_10/my-app/src/components/toDo/ToDo.js
@@ -0,0 +1,44 @@
+import * as React from 'react';
+
+export const TODO_API_ENDPOINT = 'https://jfdz12-homework-task-10-pab.firebaseio.com';
+
+export default class ToDo extends React.Component {
+    state = {
+        todoList: [],
+        error: "",
+    };
+
+    fetchToDoList = () => {
+        fetch(TODO_API_ENDPOINT)
+            .then( response => response.json())
+            .then( fetchedData => {
+                const keys = Object.keys(fetchedData);
+                const formattedData = keys.map( key => {
+                    return {
+                        id: key,
+                        ...fetchedData[key],
+                    };
+                });
+
+                this.setState({
+                    todoList: formattedData,
+                });
+            })
+            .catch( error => {
+                alert("No tasks yet, please add some tasks!");
+
+                this.setState({
+                  todoList: [],
+                  error: error,
+                });
+            });
+    };
+
+    render() {
+        return (
+            <div>
+                <h1>My "to do" list</h1>
+            </div>
+        );
+    };
+};
