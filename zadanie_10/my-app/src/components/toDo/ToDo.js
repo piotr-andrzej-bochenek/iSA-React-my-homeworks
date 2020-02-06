0dbdbd3985c23ed2bb5e34988ff2d24e6de3b536
piotr-andrzej-bochenek <piotr8207@interia.eu>
2020-02-06 23:37:45 +0100

    10.1 add table to display fetched toDos

--- zadanie_10/my-app/src/components/toDo/ToDo.js
+++ zadanie_10/my-app/src/components/toDo/ToDo.js
@@ -1,4 +1,6 @@
 import * as React from 'react';
+import { Table, Icon } from 'semantic-ui-react';
+
 import AddTask from '../addTask/AddTask';
 
 export const TODO_API_ENDPOINT = 'https://jfdz12-homework-task-10-pab.firebaseio.com';
@@ -10,7 +12,7 @@ export default class ToDo extends React.Component {
     };
 
     fetchToDoList = () => {
-        fetch(TODO_API_ENDPOINT)
+        fetch(`${TODO_API_ENDPOINT}/todo.json`)
             .then( response => response.json())
             .then( fetchedData => {
                 const keys = Object.keys(fetchedData);
@@ -35,11 +37,44 @@ export default class ToDo extends React.Component {
             });
     };
 
+    handleOnAction = () => {
+        this.fetchToDoList();
+    };
+
+    componentDidMount() {
+        this.fetchToDoList();
+    };
+
     render() {
+        const { todoList } = this.state;
+
         return (
             <div>
                 <h1>My "to do" list</h1>
-                <AddTask />
+                <Table celled structured>
+                    <Table.Header>
+                        <Table.Row>
+                        <Table.HeaderCell>Task</Table.HeaderCell>
+                        <Table.HeaderCell>Done</Table.HeaderCell>
+                        </Table.Row>
+                    </Table.Header>
+
+                    <Table.Body>
+                        {todoList.map( todo => {
+                            return (
+                                <Table.Row key={todo.id}>
+                                    <Table.Cell>{todo.task}</Table.Cell>
+                                    {todo.done !== true
+                                    ? <Table.Cell>
+                                        <Icon color='green' name='checkmark' size='large' />
+                                    </Table.Cell>
+                                    : <Table.Cell />}
+                                </Table.Row>
+                            );
+                        })}
+                    </Table.Body>
+                </Table>
+                <AddTask onAdd={this.handleOnAction} />
             </div>
         );
     };
