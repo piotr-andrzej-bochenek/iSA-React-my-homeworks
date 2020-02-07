ae1759ae8084a1d1519b2676eb97b6104de4739d
piotr-andrzej-bochenek <piotr8207@interia.eu>
2020-02-07 22:53:59 +0100

    fix firebase config misplace

--- zadanie_10/my-app/src/index.js
+++ zadanie_10/my-app/src/index.js
@@ -3,6 +3,19 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import * as firebase from 'firebase';
+
+const firebaseConfig = {
+    apiKey: "AIzaSyDR7uEPWHKD2jFrmm9-IPA-_udvLESfezs",
+    authDomain: "jfdz12-homework-task-10-pab.firebaseapp.com",
+    databaseURL: "https://jfdz12-homework-task-10-pab.firebaseio.com",
+    projectId: "jfdz12-homework-task-10-pab",
+    storageBucket: "jfdz12-homework-task-10-pab.appspot.com",
+    messagingSenderId: "905679454817",
+    appId: "1:905679454817:web:2e3d7d4ca4f1857fc22020"
+};
+
+firebase.initializeApp(firebaseConfig);
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
