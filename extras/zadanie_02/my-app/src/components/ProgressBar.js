e6a1d255441c5eca2c6431ebd14425e2c8ce4fca
piotr-andrzej-bochenek <piotr8207@interia.eu>
2020-02-09 00:15:43 +0100

    E2 add React.useEffect to ProgressBar

--- extras/zadanie_02/my-app/src/components/ProgressBar.js
+++ extras/zadanie_02/my-app/src/components/ProgressBar.js
@@ -14,6 +14,35 @@ const useOwnStyles = makeStyles( theme => ({
 
 export default function LinearBuffer() {
     const classes = useOwnStyles();
+    const [completed, setCompleted] = React.useState(0);
+    const [buffer, setBuffer] = React.useState(10);
+
+    const progress = React.useRef(() => {});
+    
+    React.useEffect(() => {
+        progress.current = () => {
+        if (completed > 100) {
+            setCompleted(0);
+            setBuffer(10);
+        } else {
+            const diff = Math.random() * 10;
+            const diff2 = Math.random() * 10;
+            setCompleted(completed + diff);
+            setBuffer(completed + diff + diff2);
+        }
+        };
+    });
+
+    React.useEffect(() => {
+        function tick() {
+        progress.current();
+        }
+        const timer = setInterval(tick, 500);
+
+        return () => {
+        clearInterval(timer);
+        };
+    }, []);
   
     return (
       <div className={classes.root}>
